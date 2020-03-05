da6bfcb31f8d47b3f5d234bb7b2bb16e7b93976c
Putra Abbas <pabbas@gmail.com>
2020-03-05 18:55:39 +0700

    material UI prep

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+    render() {
+        return(
+            <header>
+                <div className='header'>
+                    <div className="logo">
+                        <img src="http://ppia.feb.ui.ac.id/wp-content/uploads/2015/09/logoppia.png" alt="PPIA" />
+                    </div>    
+                </div>
+                
+            </header>
+        )
+    }
+}
+
+export default Header
---
+++ src/Components/Templates/TabelMahasiswa.jsx
@@ -0,0 +1,74 @@
+import React, { Component } from 'react';
+import { makeStyles, useTheme } from '@material-ui/core/styles'
+import Table from '@material-ui/core/Table'
+import TableBody from '@material-ui/core/TableBody'
+import TableCell from '@material-ui/core/TableCell'
+import TableContainer from '@material-ui/core/TableContainer'
+import TablePagination from '@material-ui/core/TablePagination'
+import TableRow from '@material-ui/core/TableRow'
+import IconButton from '@material-ui/core/IconButton'
+import FirstPageIcon from '@material-ui/icons/FirstPage'
+import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft'
+import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight'
+import LastPageIcon from '@material-ui/icons/LastPage'
+
+const useStyles1 = makeStyles(theme => ({
+    root: {
+        flexShrink: 0,
+        marginLeft: theme.spacing(2.5),
+    },
+}));
+
+function TabelMahasiswa(props) {
+    const classes = useStyles1();
+    const theme = useTheme();
+    const { count, page, rowsPerPage, onChangePage } = props;
+
+    const handleFirstPageButtonClick = event => {
+        onChangePage(event, 0);
+    };
+
+    const handleBackButtonClick = event =>  {
+        onChangePage(event, page - 1);
+    };
+
+    const handleNextButtonClick = event => {
+        onChangePage(event, page + 1);
+    };
+
+    const handleLastPageButtonClick = event => {
+        onChangePage(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
+    }
+
+    return(
+        <div className={classes.root}>
+            <IconButton
+                onClick={handleFirstPageButtonClick}
+                disabled={page === 0}
+                aria-label="awal"
+            >
+                {theme.direction === 'rtl' ? <LastPageIcon /> : <FirstPageIcon />}
+            </IconButton>
+            <IconButton onClick={handleBackButtonClick} disabled={page === 0} aria-label="prev">
+                {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
+            </IconButton>
+            <IconButton
+                onClick={handleNextButtonClick}
+                disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+                aria-label="next"
+            >
+                {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
+            </IconButton>
+            <IconButton
+                onClick={handleLastPageButtonClick}
+                disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+                aria-label="akhir"
+            >
+                {theme.direction === 'rtl' ? <FirstPageIcon /> : <LastPageIcon />}
+            </IconButton>
+        </div>
+    )
+}
+        
+
+export default TabelMahasiswa;
