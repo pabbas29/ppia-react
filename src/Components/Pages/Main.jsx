f682a5637328aef853dcae4eb8aafe05497baba6
Putra Abbas <61113904+pabbas29@users.noreply.github.com>
2020-04-02 21:52:44 +0700

    Update Main.jsx

--- src/Components/Pages/Main.jsx
+++ src/Components/Pages/Main.jsx
@@ -1,17 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import DataMhsS3 from '../DataComponent/MahasiswaS3';
 
 
-class MainPage extends Component {
-    render() {
-        return(
-            <div>
-               <h1>Hello World</h1>
-                <DataMhsS3 /> 
-            </div>
-            
-        )   
-    }
+function MainPage() {
+  return(
+      <div>
+         <h1>Hello World</h1>
+          <DataMhsS3 /> 
+      </div> 
+  )   
 }
 
 export default MainPage
